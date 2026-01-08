dc27b86eaa60ca675dfade56b10aa196f4f3da73
dien-tran <haodientran04@gmail.com>
2026-01-09 00:55:13 +0700

    Update todo (run-time server) service
    - Update server.js to connect routes and database

--- backend/services/todo_app/src/server.js
+++ backend/services/todo_app/src/server.js
@@ -3,19 +3,22 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const taskRoutes = require('./routes/task.routes');
+const planRoutes = require('./routes/plan.route');
+const taskRoutes = require('./routes/task.route');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/tasks', taskRoutes);
+app.use('/api/plans', planRoutes);
 
 app.get('/health', (req, res) => res.json({ status: 'Todo Service running', ts: new Date() }));
 
-mongoose.connect(process.env.MONGODB_URI, { dbName: 'todo' })
+mongoose.connect(process.env.MONGODB_URI, { dbName: 'todo_app' })
   .then(() => console.log('Todo Service: MongoDB Connected'))
   .catch(err => console.error('MongoDB Error:', err));
+
 
 const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => console.log(`Todo Service listening on ${PORT}`));
